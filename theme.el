0d769422c7475fd88357cf27528f58f40efc965a
Oliver Dunkl <odunkl@gmx.net>
2015-02-27 09:03:45 +0100

    Initial theme-file

---
+++ theme.el
@@ -0,0 +1,39 @@
+;; ## default
+(set-face-attribute 'region nil :background "lightgoldenrod2")
+
+;; ## notmuch
+;;(setq notmuch-search-line-faces '(("unread" . (:weight 'normal))))
+(setq notmuch-search-line-faces
+      '(("unread"  . (:weight normal))
+	("delete"  . (:background "PeachPuff"))
+	("archive" . (:background "AliceBlue"))
+	("mute"    . (:background "LemonChiffon1"))))
+(setq notmuch-tag-formats
+      '(("unread"
+	 (propertize tag 'face
+		     '(:inherit font-lock-constant-face)))
+	("archive"
+	 (propertize tag 'face
+		     '(:inherit font-lock-function-name-face)))
+	("todo"
+	 (propertize tag 'face
+		     '(:inherit font-lock-comment-face)))
+	("review"
+	 (propertize tag 'face
+		     '(:inherit font-lock-type-face)))
+	("mute"
+	 (propertize tag 'face
+		     '(:inherit font-lock-variable-name-face)))
+	("delete"
+	 (propertize tag 'face
+		     '(:inherit font-lock-comment-face)))))
+(set-face-attribute 'notmuch-search-date nil
+		    :inherit font-lock-variable-name-face)
+(set-face-attribute 'notmuch-search-count nil
+		    :inherit font-lock-type-face)
+(set-face-attribute 'notmuch-search-matching-authors nil
+		    :inherit font-lock-function-name-face)
+(set-face-attribute 'notmuch-tag-face nil
+		    :weight 'normal
+		    :foreground nil
+		    :inherit font-lock-string-face)
