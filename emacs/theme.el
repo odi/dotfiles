b260aaef335ecca4db75153edf98d261e908518c
Oliver Dunkl <oliver.dunkl@gmail.com>
2015-07-08 08:35:52 +0200

    Change face of mode-line to a darker background

--- emacs/theme.el
+++ emacs/theme.el
@@ -1,2 +1,8 @@
 ;; set background of the region
 (set-face-attribute 'region nil :background "lightgoldenrod2")
+
+;; set active mode line
+;; default fg=black; bg=grey75
+(set-face-attribute 'mode-line nil
+		    :background "gray20"
+		    :foreground "gray75")
