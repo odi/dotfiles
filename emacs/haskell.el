6e6a5f4190c1a9b9f1a50127a89bd8bff20fb033
Oliver Dunkl <oliver.dunkl@gmail.com>
2015-07-08 14:06:53 +0200

    Add haskell-decl-scan for imenu support

--- emacs/haskell.el
+++ emacs/haskell.el
@@ -1,11 +1,18 @@
 ;; ## haskell-mode
 (require 'haskell)
 (require 'haskell-indentation)
+(require 'haskell-decl-scan)
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-identation)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 
 (define-key haskell-mode-map (kbd "C-c l") 'haskell-check)
 
 (setq compile-command "nix-shell --command 'cabal build'")
 (define-key haskell-mode-map (kbd "C-c m") 'compile)
+
+;; call hoogle online w/ eww
+(defun eww-hoogle (str)
+  (interactive "sEnter hoogle search string: ")
+  (eww (concat "https://www.haskell.org/hoogle/?hoogle=" str)))
