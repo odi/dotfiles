2c940e402164c83517dedbf3152113583c87eb96
Oliver Dunkl <oliver.dunkl@gmail.com>
2015-08-25 10:07:14 +0200

    Define a working configuration for Haskell

--- emacs/haskell.el
+++ emacs/haskell.el
@@ -1,19 +1,49 @@
 ;; ## haskell-mode
 
 (require 'haskell)
-(require 'haskell-indentation)
 (require 'haskell-decl-scan)
 (require 'haskell-interactive-mode)
 (require 'haskell-process)
-(require 'haskell-unicode-input-method)
-(require 'w3m)
-(require 'w3m-haddock)
+(require 'haskell-font-lock)
+;; ;;(require 'haskell-unicode-input-method)
+;; (require 'w3m)
+;; (require 'w3m-haddock)
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; ;; turn on unicode input
+;; (require 'haskell-unicode-input-method)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-unicode-input-method)
+
+;; ## TEST ## hi2 indentation
+;; https://github.com/nilcons/hi2
+(add-to-list 'load-path "~/.emacs.d/elisp/hi2")
+(require 'hi2)
+(add-hook 'haskell-mode-hook 'turn-on-hi2)
+(setq hi2-layout-offset 4)
+
+;; use indent-guide
+(add-hook 'haskell-mode-hook 'indent-guide-mode)
+
+;; ;; haskell indentation
+;; (require 'haskell-indentation)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; (setq haskell-indentation-layout-offset 4
+;;       haskell-indentation-starter-offset 4
+;;       haskell-indentation-left-offset 4
+;;       haskell-indentation-ifte-offset 4
+;;       haskell-indentation-where-pre-offset 4
+;;       haskell-indentation-where-post-offset 4)
+
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-unicode-input-method)
 (add-hook 'haskell-mode-hook 'git-gutter-mode)
+
+;; TODO: does not work properly
+;;(add-hook 'haskell-mode-hook 'haskell-doc-current-info)
+
+(custom-set-variables
+ '(haskell-font-lock-symbols 'unicode))
+;; load file again because of not reading haskell-font-lock-symbols
+(load-file "~/.emacs.d/elisp/haskell-mode/haskell-font-lock.el")
 
 (setq haskell-process-log t)
 
@@ -23,8 +53,8 @@
 	(append (list "nix-shell" "--command")
 		(list (mapconcat 'identity argv " ")))))
 
-;; search online-hoogle instead of local hoogle-db
-(setq haskell-hoogle-command nil)
+;; ;; search online-hoogle instead of local hoogle-db
+;; (setq haskell-hoogle-command nil)
 
 ;; indentation configure
 (setq haskell-indentation-layout-offset 4
@@ -43,3 +73,16 @@
 ;; (defun eww-hoogle (str)
 ;;   (interactive "sEnter hoogle search string: ")
 ;;   (eww (concat "https://www.haskell.org/hoogle/?hoogle=" str)))
+
+;; turn on presentation-mode for C-c C-i and C-c C-t
+(setq haskell-process-use-presentation-mode t)
+(defun haskell-quit-and-clear-presentation ()
+  (interactive)
+  (haskell-presentation-clear)
+  (quit-window))
+;; delete and quit presentation-window with 'Q'
+(define-key haskell-presentation-mode-map (kbd "Q")
+  'haskell-quit-and-clear-presentation)
+
+;; keyboard shortcuts
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
