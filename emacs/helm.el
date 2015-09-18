759c1a55cc65d99eb4ba7556497f87404488633f
Oliver Dunkl <oliver.dunkl@gmail.com>
2015-09-18 14:03:55 +0200

    Add ag as search command
    Use helm-ag to search with ag in helm.

--- emacs/helm.el
+++ emacs/helm.el
@@ -22,6 +22,14 @@
 (global-set-key (kbd "C-c h p") 'helm-projectile)
 (global-set-key (kbd "C-c h g") 'helm-projectile-grep)
 
+;; ## ag and helm-ag
+(add-to-list 'load-path "~/.emacs.d/elisp/emacs-helm-ag")
+(require 'ag)
+(require 'helm-ag)
+(setq helm-ag-insert-at-point 'symbol)
+(global-set-key (kbd "C-c h a") 'helm-do-ag-project-root)
+(global-set-key (kbd "C-c h A") 'helm-do-ag-this-file) ;; posible replaces helm-swoop?
+
 ;; quickfix for helm+projectile error message
 ;; https://github.com/bbatsov/projectile/issues/358
 (defalias 'helm-buffers-match-function 'helm-buffers-list--match-fn)
